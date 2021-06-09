6d3ed8e14348ebf1f07448ae2208c6ae3f5510c2
Marcelo <marceloindart@gmail.com>
2021-06-09 15:11:25 +0000

    new work

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,70 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "../../styles/navbar.scss";
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<div>
+			<header>
+				{/*- Navbar -*/}
+				<nav className="navbar navbar-expand-lg">
+					<div className="container">
+						<a className="navbar-brand text-white" href="#">
+							<i className="fas fa-camera-retro" />
+							Juan H. Photography
+						</a>
+						<button
+							className="navbar-toggler"
+							type="button"
+							data-toggle="collapse"
+							data-target="#nvbCollapse"
+							aria-controls="nvbCollapse">
+							<span className="navbar-toggler-icon" />
+						</button>
+						<div className="collapse navbar-collapse" id="nvbCollapse">
+							<ul className="navbar-nav ml-auto">
+								<li className="nav-item pl-1">
+									<a className="nav-link" href="/">
+										<i className="fa fa-home fa-fw mr-1" />
+										Home
+									</a>
+								</li>
+								<li className="nav-item active pl-1">
+									<a className="nav-link" href="#">
+										<i className="fa fa-th-list fa-fw mr-1" />
+										Blog
+									</a>
+								</li>
+								<li className="nav-item pl-1">
+									<a className="nav-link" href="/gallery">
+										<i className="fa fa-info-circle fa-fw mr-1" />
+										Gallery
+									</a>
+								</li>
+								{/* <li className="nav-item pl-1">
+									<a className="nav-link" href="/contact">
+										<i className="fa fa-phone fa-fw fa-rotate-180 mr-1" />
+										Contact
+									</a>
+								</li> */}
+								<li className="nav-item pl-1">
+									<a className="nav-link" href="/demo">
+										<i className="fa fa-user-plus fa-fw mr-1" />
+										About me
+									</a>
+								</li>
+								<li className="nav-item pl-1">
+									<a className="nav-link" href="#">
+										<i className="fa fa-sign-in fa-fw mr-1" />
+										Other links
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+				{/*# Navbar #*/}
+			</header>
+			{/*- Footer -*/}
+		</div>
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,101 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div>
+		<div>
+			<div>
+				<div id="slider">
+					<figure className="slide_img">
+						<img
+							src="https://m.22slides.com/jhernandez/anrepark231217-14-6230.jpg?auto=format&w=1200&s=227975a1771859cca0c14d380e842df1"
+							alt="Juicy"
+							width={1665}
+							height={800}
+						/>
+						{/* <figcaption>Can be Juicy !</figcaption> */}
+					</figure>
+					<figure className="slide_img">
+						<img
+							src="https://m.22slides.com/jhernandez/20200812-dscf1067-5043.jpg?auto=format&w=1000&s=84339576f0622c1dd4cc44f349cae0a3"
+							alt="Bridge"
+							width={1665}
+							height={800}
+						/>
+						{/* <figcaption>Iron piece between two part of land</figcaption> */}
+					</figure>
+					<figure className="slide_img">
+						<img
+							src="https://m.22slides.com/jhernandez/20200917-dscf2468-1017.jpg?auto=format&w=1000&s=385b92109fa43d424e096a8c4f960587"
+							alt="Taking pics"
+							width={1665}
+							height={800}
+						/>
+						{/* <figcaption>Photography is magic</figcaption> */}
+					</figure>
+				</div>
+			</div>
+		</div>
+		<figure className="snip1336">
+			<img
+				src="https://m.22slides.com/jhernandez/sonyrx100vi9-6878.jpg?auto=format&w=800&s=d25b5d75b9ed6af660da40a435c50299"
+				alt="sample87"
+			/>
+			<figcaption>
+				<img
+					src="https://scontent-mia3-2.xx.fbcdn.net/v/t1.6435-9/102414262_10158526496309468_7766513217142245399_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=174925&_nc_ohc=y2VS0Jf7w-IAX-8ZeHp&_nc_oc=AQmjrePG_p_vnocaeg-dAp8ATtv5wYrK143N5xeBoPggoFY4HLJw9gas0PH6GVAhv8agychG15OKRsK46OO0425S&_nc_ht=scontent-mia3-2.xx&oh=7d7899980fc72f934314b7cd90bfb9ba&oe=60E6BCEB"
+					alt="profile-sample4"
+					className="profile"
+				/>
+				<h2>
+					Juan Hernandez
+					<span>Photagrapther</span>
+				</h2>
+				<p>
+					Thank you for visiting my page. I am an experienced photographer who has worked as a music
+					photojournalist and a general life photographer for MANY years, you can see that work In this link
+					to my other page on this button
+				</p>
+				<a href="/demo" className="follow">
+					More Bio
+				</a>
+				<a href="https://www.facebook.com/Juanpointeight" className="info">
+					Fallow
+				</a>
+			</figcaption>
+		</figure>
+		<div className="wrapper">
+			<div className="item">
+				<div className="fuji">
+					<img src="https://image.ibb.co/b8UJBc/administration_architecture_big_ben_221166.jpg" />
+					<div className="caption">I Miss London</div>
+				</div>
+			</div>
+			<div className="item">
+				<div className="fuji">
+					<img src="https://image.ibb.co/mmyvrc/anniversary_balloons_birthday_68369.jpg" />
+					<div className="caption">Love ballons</div>
+				</div>
+			</div>
+			<div className="item">
+				<div className="fuji">
+					<img src="https://image.ibb.co/hQaarc/antique_blur_camera_828378.jpg" />
+					<div className="caption">Vintage life </div>
+				</div>
+			</div>
+			<div className="item">
+				<div className="fuji">
+					<img src="https://image.ibb.co/crFarc/pexels_photo_100756.jpg" />
+					<div className="caption">Summer Day</div>
+				</div>
+			</div>
+			<div className="item">
+				<div className="fuji">
+					<img src="https://image.ibb.co/fvekrc/action_adult_art_673649.jpg" />
+					<div className="caption">Art</div>
+				</div>
+			</div>
+		</div>
 	</div>
 );
